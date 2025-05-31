eca18969b3369103092fc29ccb70db5d391765fa
Jedad11 <jedsadice@gmail.com>
2025-05-31 14:36:48 +0700

    choice route done

---
+++ BackEnd/src/routes/choice.route.ts
@@ -0,0 +1,6 @@
+import { Hono } from "hono";
+import * as choiceController from "../controllers/choice.controller.ts"
+
+const choiceRouter = new Hono()
+
+choiceRouter.post("/:choiceId/:examScoreId", choiceController.checkChoice)
