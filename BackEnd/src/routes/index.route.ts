e7542a986b73e689e97f9267c709e9538a6d9280
Jedad11 <jedsadice@gmail.com>
2025-05-31 16:30:13 +0700

    exam score route done

--- BackEnd/src/routes/index.route.ts
+++ BackEnd/src/routes/index.route.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import userRouter from "./user.route.ts";
 import examRouter from "./exam.route.ts";
+import examScoreRouter from "./examScore.route.ts";
 
 const mainRouter = new Hono()
 
 mainRouter.route("/users", userRouter)
 mainRouter.route("/exams", examRouter)
+mainRouter.route("/examScores", examScoreRouter)
 
 export default mainRouter
